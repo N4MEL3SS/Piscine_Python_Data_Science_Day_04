c9fd71099137b16dddda91e26abeb5403092e62b
celadia <celadia@21-school.ru>
2022-09-21 20:33:00 +0300

    finish work

--- ex05/generator.py
+++ ex05/generator.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 from sys import argv
+import psutil
 
 
-def main(file):
-    pass
+def generator(filepath):
+    with open(filepath, 'r') as file:
+        for line in file:
+            yield line
+
+
+def main(filepath):
+    try:
+        for line in generator(filepath):
+            pass
+    except FileNotFoundError as err:
+        print(err)
+
+    mem = psutil.Process().memory_info().vms / (10 ** 9)
+    cpu = psutil.Process().cpu_times()
+    print(f'Peak Memory Usage = {mem} Gb')
+    print(f'User Time + System Time = {cpu.user + cpu.system}s')
 
 
 if __name__ == '__main__':
     if len(argv) == 2:
         main(argv[1])
     else:
-        print('Usage: python3 generator.py filepath')
+        print('Usage: python3 ./generator.py filepath')
--- ex05/ordinary.py
+++ ex05/ordinary.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 from sys import argv
+import psutil
 
 
-def main(file):
-    pass
+def ordinary(filepath):
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
+    return lines
+
+
+def main(filepath):
+    try:
+        for line in ordinary(filepath):
+            pass
+    except FileNotFoundError as err:
+        print(err)
+
+    mem = psutil.Process().memory_info().vms / (10 ** 9)
+    cpu = psutil.Process().cpu_times()
+    print(f'Peak Memory Usage = {mem} Gb')
+    print(f'User Time + System Time = {cpu.user + cpu.system}s')
 
 
 if __name__ == '__main__':
     if len(argv) == 2:
         main(argv[1])
     else:
-        print('Usage: python3 ordinary.py filepath')
+        print('Usage: python3 ./ordinary.py filepath')
